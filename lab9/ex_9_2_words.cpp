ade0c5848d651126f43bf2fd3b269783bae6296b
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-05-15 17:39:14 +0200

    done lab9ex2

--- lab9/ex_9_2_words.cpp
+++ lab9/ex_9_2_words.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <map>
+#include <fstream>
 
 /**
  * Removes all non alphanumeric characters from given word and converts to lower case.
  * @param[in,out] word on return word consist only of lower case characters.
  */
-void toLowerAlpha(std::string & s1) ;
+void toLowerAlpha(std::string & s1) {
+    std::transform(s1.begin(), s1.end(), s1.begin(),
+                   [](char c) { return std::tolower(c); });
+    s1.erase(std::remove_if(s1.begin(),
+                            s1.end(),
+                            [](unsigned char c) { return !std::isalnum(c); }),
+             s1.end());
+}
 
 int main(){
     int count = 0;
     std::string word;
-    map<string, int> c;
-    std::vector<int> v;
-    while( cin >> word) {
+    std::map<std::string, int> m;
+
+    std::fstream file("/Users/jakubstepien/Desktop/6-semestr/EMCPP/zadania-rozwiazania/lab9/hamletEN.txt");
+
+    while( file >> word) {
        toLowerAlpha(word);
 
-       if(word != ""){
-           c[word]++;
-           count++;
+       if(!word.empty()){
+           if(m[word]++ == 0) {
+               count++;
+           }
        }
-
     }
-    // ...
-    multimap<int, string> m2;
+    std::multimap<int, std::string, std::greater<>> m2;
 
-    cout << "Number of distinct words : " << count << endl;
-    cout << "\nThe top 20 most popular words: \n";
-    // ...
+    std::for_each(m.begin(), m.end(), [&m2](const std::pair<std::string, int>& p){
+        m2.emplace(p.second, p.first);
+    });
+
+    std::cout << "Number of distinct words : " << count << std::endl;
+    std::cout << "\nThe top 20 most popular words: \n";
+
+    std::for_each(m2.begin(), std::next(m2.begin(), 20), [](auto p){
+        std::cout << p.second << " : " << p.first << std::endl;
+    });
+
     return 0;
 }
 /*
