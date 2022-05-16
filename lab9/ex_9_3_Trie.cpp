dfd65990207b4e9c67b88c70d0c11d379c307038
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-05-16 20:08:25 +0200

    done lab9ex3

--- lab9/ex_9_3_Trie.cpp
+++ lab9/ex_9_3_Trie.cpp
@@ -1,36 +1,78 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 using namespace std;
-/**
- * Trie structure
- *
- * HINT: use std::map to implement it!
- *       My implementation adds less than 25 lines of code. 
- */
+
 class Trie{
-  /// TODO:
+    std::map<std::string, Trie> data;
+
+    void addSentence(const std::vector<std::string>  & sentence, int i=0) {
+        if(sentence.size() > i) {
+            auto search = data.find(sentence[i]);
+
+            // element already in map
+            if (search != data.end()) {
+                search->second.addSentence(sentence, i+1);
+            } else {
+                Trie newTrie = {};
+                newTrie.addSentence(sentence, i+1);
+                data.emplace(sentence[i], newTrie);
+            }
+        }
+    }
+
+    void printEnding(std::string result) {
+        if(!data.empty()) {
+            std::for_each(data.begin(), data.end(), [result](auto p){
+                std:string tmp = result;
+                tmp.append(p.first);
+                tmp.append(" ");
+                p.second.printEnding(tmp);
+            });
+        } else {
+            std::cout << result << std::endl;
+        }
+    }
+
+    void findLastElementOfBeginningSentence(const std::vector<std::string>& beginningOfSentence, int i) {
+        if(beginningOfSentence.size() > i) {
+            auto search = data.find(beginningOfSentence[i]);
+
+            if (search != data.end()) {
+                if(beginningOfSentence.size()-1 == i)
+                    search->second.printEnding("");
+                else
+                    search->second.findLastElementOfBeginningSentence(beginningOfSentence, i + 1);
+            }
+        }
+    }
+
 public:
-    static void printSentence(const std::vector<std::string>  & sentence ){
-        for(const auto & w : sentence)
+    static void printSentence(const std::vector<std::string>  & sentence ) {
+        for (const auto &w: sentence)
             cout << w << " ";
     }
-    void add(const std::vector<std::string>  & sentece ){
-       cout << "Adding : ";
-       printSentence(sentece);
-       cout << "\n";
-       /// TODO:
+
+    void add(const std::vector<std::string>  & sentence ){
+        cout << "Adding : ";
+        printSentence(sentence);
+        cout << "\n";
+
+        addSentence(sentence);
     }
-    void printPossibleEndings(const std::vector<std::string>  & beginningOfSentece ){
+
+    void printPossibleEndings(const std::vector<std::string>& beginningOfSentence ){
         cout << "Endings for \"";
-        printSentence(beginningOfSentece);
+        printSentence(beginningOfSentence);
         cout << "\" are :\n";
-        // TODO:
+        findLastElementOfBeginningSentence(beginningOfSentence, 0);
     }
+
     void load(const std::string & fileName){
         ifstream file(fileName);
         if(!file){
-            cerr << "Error when openning file " << fileName << endl;
+            cerr << "Error when opening file " << fileName << endl;
             return;
         }
         string word;
@@ -48,8 +90,9 @@ public:
 
 
 int main(){
+
     Trie dictionary;
-    dictionary.load("../sample.txt");
+    dictionary.load("/Users/jakubstepien/Desktop/6-semestr/EMCPP/zadania-rozwiazania/lab9/sample.txt");
     //dictionary.load("hamletEN.txt");
 
     dictionary.printPossibleEndings({"Curiosity"});
